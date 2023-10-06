22b763b300c9a3d82cfcfa76301b8e16bcde5261
Charbel Gérmán López <142644844+Charbel-ai@users.noreply.github.com>
2023-10-06 20:11:23 +0000

    cambios

--- ejerciciodeesteban.cpp
+++ ejerciciodeesteban.cpp
@@ -14,15 +14,15 @@ std::cout<<"------Convertidor de escalas------"<<std::endl;
 std::cout<<"Ingresa la temepratura que quieres convertir, solo los numeros"<<std::endl;
 std::cin>>temperatura;
 do{
-std::cout<<"Ingresa la temperatura original de tu medida (1= Fahrenheit , 2= Celsius , 3= Kelvin )"<<std::endl;
+std::cout<<"Ingresa la temperatura original de tu medida, segun los numeros que se les asignaron (1= Fahrenheit , 2= Celsius , 3= Kelvin )"<<std::endl;
 std::cin>>medida;
 if ((medida != 1) && (medida != 2)&&(medida != 3)){
-    std::cout<<"Ingresa una medida de teperatura valida "<<std::endl;}
+    std::cout<<"Ingresa una medida de teperatura valida"<<std::endl;}
 }
 while((medida != 1) && (medida != 2)&&(medida != 3));
 
 do{
-std::cout<<"Ingresa la temperatura a la que la quieres convertir (4= Fahrenheit , 5= Celsius , 6= Kelvin )"<<std::endl;
+std::cout<<"Ingresa la temperatura a la que la quieres convertir, segun los numeros que se les asignaron (4= Fahrenheit , 5= Celsius , 6= Kelvin )"<<std::endl;
 std::cin>>medida2;
 if ((medida2 != 4) && (medida2 != 5)&&(medida2 != 6)){
     std::cout<<"Ingresa una medida de teperatura valida "<<std::endl;}
@@ -40,7 +40,14 @@ else if ((medida == 1)&&(medida2 == 4)){
 std::cout<<"Has escogido la misma temperatura, no has convertido tu temperatura, sigue siendo "<< temperatura << " Fahrenheit" << std::endl;}
 
 //Conversion de Celsius 
+if((medida == 2)&&(medida2 == 4) ){
+std::cout<<"Tu temperatura de Celsius, es igual a "<< 5 * (temperatura - 32) / 9<< " Celsius ";}
 
+else if ((medida == 2)&&(medida2 == 6)){
+std::cout<<"Tu temperatura de Fahrenheit, es igual a "<< 5 * (temperatura - 32) / 9 + 273.15 << " Celsius ";}
+
+else if ((medida == 2)&&(medida2 == 5)){
+std::cout<<"Has escogido la misma temperatura, no has convertido tu temperatura, sigue siendo "<< temperatura << " Fahrenheit" << std::endl;}
 
    
    
