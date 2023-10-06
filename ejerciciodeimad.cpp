2bbe0921eda1eeb695624f4ab8110ce0ed0f2b30
Charbel Gérmán López <142644844+Charbel-ai@users.noreply.github.com>
2023-10-06 21:22:43 +0000

    Avances hasta el momento

--- ejerciciodeimad.cpp
+++ ejerciciodeimad.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <math.h>
 //Crear un programa q resuelva ecuaciones cuadráticas, en una misma línea 
 //mandar los valores de a,b y c y se deben de imprimir las dos raíces 
 //(resultados) al igual q la ecuación  y= ax2+ bx + c con los valores de (a,b y c 
 //ejemplo “y= 2x2 + 3x + 5”)
 int main (){
+
+int a;
+int b; 
+int c; 
+
+std::cout<<"---Suma de una ecuacion cuadratica---"<<std::endl;
+std::cout<<"Ingrese el valor de a"<<std::endl;
+std::cin>>a; 
+std::cout<<"Ingrese el valor de b"<<std::endl;
+std::cin>>b; 
+std::cout<<"Ingrese el valor de c"<<std::endl;
+std::cin>>c; 
+std::cout<<"La ecuacion incial es y = "<< a<< "x2 + "<< b<< "x + "<< c <<std::endl;
+std::cout<<""<<std::endl;
+//std::cout<<"X1 = "<< (-b ())<<std::endl;
 
 
 
